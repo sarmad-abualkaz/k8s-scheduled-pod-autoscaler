4e7fe6995356c95e06ceb1f668d78c835e7a0411
sarmad-abualkaz <sarmad.abualkaz@prodigygame.com>
2021-03-21 22:22:52 -0400

    adding webhook and moving to controller-runtime v0.7.0

--- api/v1/scheduledpodautoscaler_webhook.go
+++ api/v1/scheduledpodautoscaler_webhook.go
@@ -17,9 +17,14 @@ limitations under the License.
 package v1
 
 import (
-	"k8s.io/apimachinery/pkg/runtime"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"time"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
@@ -42,7 +47,10 @@ var _ webhook.Defaulter = &ScheduledPodAutoscaler{}
 func (r *ScheduledPodAutoscaler) Default() {
 	scheduledpodautoscalerlog.Info("default", "name", r.Name)
 
-	// TODO(user): fill in your defaulting logic.
+	// default 'Spec.Resource.Type' to 'deployment' if set blank
+	if r.Spec.Resource.Type == "" {
+		r.Spec.Resource.Type = "deployment"
+	}
 }
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
@@ -55,7 +63,7 @@ func (r *ScheduledPodAutoscaler) ValidateCreate() error {
 	scheduledpodautoscalerlog.Info("validate create", "name", r.Name)
 
 	// TODO(user): fill in your validation logic upon object creation.
-	return nil
+	return r.validateScheduledPodAutoscaler()
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
@@ -63,7 +71,7 @@ func (r *ScheduledPodAutoscaler) ValidateUpdate(old runtime.Object) error {
 	scheduledpodautoscalerlog.Info("validate update", "name", r.Name)
 
 	// TODO(user): fill in your validation logic upon object update.
-	return nil
+	return r.validateScheduledPodAutoscaler()
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
@@ -71,5 +79,52 @@ func (r *ScheduledPodAutoscaler) ValidateDelete() error {
 	scheduledpodautoscalerlog.Info("validate delete", "name", r.Name)
 
 	// TODO(user): fill in your validation logic upon object deletion.
+	return nil
+}
+
+func (r *ScheduledPodAutoscaler) validateScheduledPodAutoscaler() error {
+	var allErrs field.ErrorList
+	if err := r.validateScheduledPodAutoscalerSpec(); err != nil {
+		allErrs = append(allErrs, err)
+	}
+
+	if err := r.validateScheduledPodAutoscalerTimeEnteries(); err != nil {
+		allErrs = append(allErrs, err)
+	}
+
+	if len(allErrs) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(
+		schema.GroupKind{Group: "autoscaling.spa.sarmadabualkaz.io", Kind: "ScheduledPodAutoscaler"},
+		r.Name, allErrs)
+}
+
+func (r *ScheduledPodAutoscaler) validateScheduledPodAutoscalerSpec() *field.Error {
+	// The field helpers from Kubernetes API machinery to return
+	// structured validation errors
+
+	if r.Spec.Resource.Name == "" {
+		return field.Invalid(field.NewPath("spec").Child("resource").Key("name"), r.Spec.Resource.Name, "name cannot be blank and must be no more than 52 characters")
+	} else if *r.Spec.ScaleDown.Value <= 0 {
+		return field.Invalid(field.NewPath("spec").Child("scaleDown").Key("value"), r.Spec.ScaleDown.Value, "scalueDown.value is invalid - needs to be at least equal to 1")
+	} else if *r.Spec.ScaleUp.Value <= *r.Spec.ScaleDown.Value {
+		return field.Invalid(field.NewPath("spec").Child("scaleUp").Key("value"), r.Spec.ScaleUp.Value, "scalueUp.value is invalid - needs to be more than scaleDown.value")
+	}
+	return nil
+}
+
+func (r *ScheduledPodAutoscaler) validateScheduledPodAutoscalerTimeEnteries() *field.Error {
+	// The field helpers from Kubernetes API machinery to return
+	// structured validation errors
+
+	//check if time is validly entred
+	if _, err := time.Parse(time.Kitchen, r.Spec.ScaleUp.Time); err != nil {
+		return field.Invalid(field.NewPath("spec").Child("scaleUp").Key("time"), r.Spec.ScaleUp.Time, err.Error())
+	}
+	if _, err := time.Parse(time.Kitchen, r.Spec.ScaleDown.Time); err != nil {
+		return field.Invalid(field.NewPath("spec").Child("scaleDown").Key("time"), r.Spec.ScaleDown.Time, err.Error())
+	}
 	return nil
 }
